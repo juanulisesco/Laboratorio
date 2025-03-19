b57e4df82b2fc10b272e932a2e8c841a9d9cf0dc
juanulisesco <alumno26.copati.hugue.ulises@ipm.edu.ar>
2025-03-19 09:27:48 -0300

    Update ejercicio3.cpp

--- Repaso c++/ejercicio3.cpp
+++ Repaso c++/ejercicio3.cpp
@@ -29,36 +29,41 @@ void pedido(vector<Producto> productos, vector<Pedido> &pedidos){
             cin >>aux.nombre;
             cout<<"Ingresar el apellido del cliente"<<endl;
             cin >>aux.apellido;
-            cout<<"codigo:0, Milanesa con papas 20500$"
-                  "codigo:1, Canelones 8500$"
-                  "codigo:2, Ñoquis con salsa 18000$"
-                  "codigo:3, Fideos con albondigas 27800$"
-                  "codigo:4, Risotto 40000$"<<endl;
+            cout<<"codigo:0, Milanesa con papas 20500$"<<endl;
+            cout<<"codigo:1, Canelones 8500$"<<endl;
+            cout<<"codigo:2, Ñoquis con salsa 18000$"<<endl;
+            cout<<"codigo:3, Fideos con albondigas 27800$"<<endl;
+            cout<<"codigo:4, Risotto 40000$"<<endl;
             cin >>gome;
             if(gome == 0){
-                productos[gome]= aux.lista_producto;
+                aux.lista_producto.nombre_producto = productos[gome].nombre_producto;
                 cout<<"Ingrese la cantidad del producto deseado: "<<endl;
                 cin >>aux.lista_producto.cantidad;
+                aux.lista_producto.precio= aux.lista_producto.cantidad * productos[gome].precio;
             }
             else if(gome == 1){
-                productos[gome]= aux.lista_producto;
+                aux.lista_producto.nombre_producto = productos[gome].nombre_producto;
                 cout<<"Ingrese la cantidad del producto deseado: "<<endl;
                 cin >>aux.lista_producto.cantidad;
+                aux.lista_producto.precio= aux.lista_producto.cantidad * productos[gome].precio;
             }
             else if(gome == 2){
-                productos[gome]= aux.lista_producto;
+                aux.lista_producto.nombre_producto = productos[gome].nombre_producto;
                 cout<<"Ingrese la cantidad del producto deseado: "<<endl;
                 cin >>aux.lista_producto.cantidad;
+                aux.lista_producto.precio= aux.lista_producto.cantidad * productos[gome].precio;
             }
             else if(gome == 3){
-                productos[gome]= aux.lista_producto;
+                aux.lista_producto.nombre_producto = productos[gome].nombre_producto;
                 cout<<"Ingrese la cantidad del producto deseado: "<<endl;
                 cin >>aux.lista_producto.cantidad;
+                aux.lista_producto.precio= aux.lista_producto.cantidad * productos[gome].precio;
             }
             else if(gome == 4){
-                productos[gome]= aux.lista_producto;
+                aux.lista_producto.nombre_producto = productos[gome].nombre_producto;
                 cout<<"Ingrese la cantidad del producto deseado: "<<endl;
                 cin >>aux.lista_producto.cantidad;
+                aux.lista_producto.precio= aux.lista_producto.cantidad * productos[gome].precio;
             }
 
             pedidos.push_back(aux);
@@ -68,24 +73,31 @@ void pedido(vector<Producto> productos, vector<Pedido> &pedidos){
         
     }
 
-
-    
-    void cancelar(){
-
+    void cancelar(vector<Pedido> &pedidos, int cance){
+        pedidos.erase(pedidos.begin() + cance);
+        cout<<"---------------------------------------"<<endl;
     };
 
     void ver(vector<Pedido> pedidos){
         for(int i= 0; i<pedidos.size(); i++){
-        cout<<pedidos[i].nombre<<endl;
-        cout<<pedidos[i].apellido<<endl;
-        cout<<pedidos[i].lista_producto.nombre_producto<<endl;
-        cout<<pedidos[i].lista_producto.cantidad<<endl;
-        cout<<pedidos[i].lista_producto.precio<<endl;
+        cout<<"---------------------------------------"<<endl;    
+        cout<<"Nombre: "<<pedidos[i].nombre<<endl;
+        cout<<"Apellido: "<<pedidos[i].apellido<<endl;
+        cout<<"Nombre del producto: "<<pedidos[i].lista_producto.nombre_producto<<endl;
+        cout<<"Cantidad pedida: "<<pedidos[i].lista_producto.cantidad<<endl;
+        cout<<"Precio total del pedido: "<<pedidos[i].lista_producto.precio<<endl;
+        cout<<"---------------------------------------"<<endl;
         }
     };
 
-    void total(){
-
+    void total(vector<Pedido> pedidos){
+        int papugomemax= 0;
+        for (int i = 0; i<pedidos.size(); i++){
+            papugomemax= papugomemax + pedidos[i].lista_producto.precio;
+        }
+        cout<<"---------------------------------------"<<endl;
+        cout<<"El total ganado es: "<<papugomemax<<endl;
+        cout<<"---------------------------------------"<<endl;
     };
 
 int main(){
@@ -94,23 +106,27 @@ int main(){
     vector<Producto> productos= {{"Milanesa con papas ",0 ,20500}, {"Canelones ",1 ,8500}, {"Ñoquis con salsa blanca",2 ,18000}, {"Fideos con albondigas",3 ,27800}, {"Risotto ",4 ,40000}};
 
     while(numero!= 5){
-        cout<< "Escriba 1 si quiere hacer un pedido"
-               "Escriba 2 si quiere cancelar un pedido"
-               "Escriba 3 si quiere ver los pedidos en curso"
-               "Escriba 4 si quiere ver el total ganado en el dia"
-               "Escriba 5 si quiere detener el programa"<<endl;
+        cout<< "Escriba 1 si quiere hacer un pedido"<<endl;
+        cout<< "Escriba 2 si quiere cancelar un pedido"<<endl;
+        cout<< "Escriba 3 si quiere ver los pedidos en curso"<<endl;
+        cout<< "Escriba 4 si quiere ver el total ganado en el dia"<<endl;
+        cout<< "Escriba 5 si quiere detener el programa"<<endl;
         cin >>numero;
         if(numero== 1){
             pedido(productos, pedidos);
         }
         else if(numero== 2){
-            cancelar();
+            int cance= 0;
+            cout<<"---------------------------------------"<<endl;
+            cout<<"Ingrese el pedido que desea eliminar: "<<endl;
+            cin >>cance;
+            cancelar(pedidos, cance);
         }
         else if(numero== 3){
             ver(pedidos);
         }
         else if(numero== 4){
-            total();
+            total(pedidos);
         }
 
     }
